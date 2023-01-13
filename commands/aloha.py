dd5a1c1c0144cd126c999ca2721c74e0325cd477
alexanderthegreat96 <alexandrulupaescu@gmail.com>
2023-01-13 10:33:34 -0800

    Appended another commandfile for the commands.json

--- commands/aloha.py
+++ commands/aloha.py
@@ -1,4 +1,7 @@
-class help:
+import discord
+import asyncio
+
+class aloha:
     def __init__(self, bot, ctx, args, authorization, inputArguments):
         self.bot = bot
         self.ctx = ctx
@@ -6,5 +9,7 @@ class help:
         self.args = args
         self.inputArguments = inputArguments
 
+
     async def main(self):
-        await self.ctx.channel.send("```This is the help.py command output within commands/dth folder.```")
+        await self.ctx.channel.send("```This is the aloha command output within commands folder.```")
+    
